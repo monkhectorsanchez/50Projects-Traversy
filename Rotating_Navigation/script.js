54d2adf565b7624ee5150f1c7f3eb94ebef59c23
Hector <monkhectorsanchez@gmail.com>
2022-05-31 19:43:34 -0400

    fixed circle positioning issue. Had to restructure html, change styling, and add javascript.

--- Rotating_Navigation/script.js
+++ Rotating_Navigation/script.js
@@ -1,7 +1,20 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const content = document.querySelector('.content');
+const circle = document.querySelector('.circle');
 
-open.addEventListener('click', () => container.classList.add('show-nav'));
+open.addEventListener('click', () => {
+  container.classList.add('show-nav');
+  circle.classList.add('show-nav');
+});
 
-close.addEventListener('click', () => container.classList.remove('show-nav'));
+close.addEventListener('click', () => {
+  container.classList.remove('show-nav');
+  circle.classList.remove('show-nav');
+});
+
+container.addEventListener('click', () => {
+  container.classList.remove('show-nav');
+  circle.classList.remove('show-nav');
+});
