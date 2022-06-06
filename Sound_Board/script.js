e53be1f4b4b174f888f723c11aa76ac15a02e863
Hector <monkhectorsanchez@gmail.com>
2022-06-06 18:25:20 -0400

    added the rest of the functionality

--- Sound_Board/script.js
+++ Sound_Board/script.js
@@ -6,5 +6,20 @@ sounds.forEach(sound => {
 
   btn.innerText = sound
 
+  btn.addEventListener('click', () => {
+    stopsSongs()
+
+    document.getElementById(sound).play()
+  })
+
   document.getElementById('buttons').appendChild(btn);
 })
+
+function stopsSongs() {
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime = 0;
+  })
+}
